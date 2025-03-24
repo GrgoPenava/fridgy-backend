d69c733b2b1b68398df0dbdba46d0e5367a9ce7d
GrgoPenava <grgopenava00@gmail.com>
2025-03-24 23:37:57 +0100

    Add some user routes

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { getAllUsers } from "./controller";
-import { usersSchema } from "./schema";
+import { deleteUserById, getAllUsers, getUserById } from "./controller";
+import { deleteUserSchema, userSchema, usersSchema } from "./schema";
 import { authenticate } from "../../middleware/auth";
 
 export async function usersRoutes(app: FastifyInstance) {
@@ -13,4 +13,28 @@ export async function usersRoutes(app: FastifyInstance) {
     { schema: getFridgeSchema, preHandler: authenticate },
     getFridgeController
   ); */
+
+  app.get<{ Params: { id: string } }>(
+    "/user/:id",
+    {
+      schema: {
+        ...userSchema,
+        tags: ["User"],
+      },
+      preHandler: authenticate,
+    },
+    getUserById
+  );
+
+  app.delete<{ Params: { id: string } }>(
+    "/:id",
+    {
+      schema: {
+        ...deleteUserSchema,
+        tags: ["User"],
+      },
+      preHandler: authenticate,
+    },
+    deleteUserById
+  );
 }
--- src/modules/user/service.ts
+++ src/modules/user/service.ts
@@ -8,6 +8,7 @@ export async function getUsers(): Promise<Response> {
       select: {
         id: true,
         email: true,
+        username: true,
         fridges: {
           select: {
             id: true,
@@ -35,6 +36,74 @@ export async function getUsers(): Promise<Response> {
     logger.error({ error }, "Greška pri dohvaćanju korisnika");
     return {
       message: "Greška pri dohvaćanju korisnika",
+      code: 500,
+    };
+  }
+}
+
+export async function getUser(id: number): Promise<Response> {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        fridges: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      logger.warn({ id }, "User nije pronađen");
+      return {
+        message: "User nije pronađen",
+        code: 404,
+      };
+    }
+
+    logger.info({ userId: id }, "User dohvaćen");
+    console.log("DADADADADADA -", user);
+
+    return {
+      message: "User uspješno dohvaćen",
+      code: 200,
+      data: user,
+    };
+  } catch (error) {
+    logger.error({ error, id }, "Greška pri dohvaćanju usera");
+    return {
+      message: "Greška pri dohvaćanju usera",
+      code: 500,
+    };
+  }
+}
+
+export async function deleteUser(id: number): Promise<Response> {
+  try {
+    const user = await prisma.user.delete({
+      where: { id },
+      select: {
+        username: true,
+        email: true,
+        id: true,
+      },
+    });
+
+    logger.info({ userId: id }, "User obrisan");
+    return {
+      message: "User uspješno obrisan",
+      code: 200,
+      data: user,
+    };
+  } catch (error) {
+    logger.error({ error, id }, "Greška pri brisanju usera");
+    return {
+      message: "Greška pri brisanju usera",
       code: 500,
     };
   }
